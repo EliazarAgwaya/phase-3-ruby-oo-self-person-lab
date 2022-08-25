d070136d7cf53ca575ab8c25635fab5e0263cf55
EliazarAgwaya <eliazar.magero@student.moringaschool.com>
2022-08-25 09:20:24 +0300

    commit tree b917709be16ad2900eee38e423d6535c856f1949

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,28 @@
 # your code goes here
+class Person
+    attr_reader :name, :bank_account, :happiness, :hygiene
+    attr_accessor :hygiene
+    def initialize (name)
+        @name=name
+        @bank_account=25
+        @happiness=8
+        @hygiene=8
+    end
+
+    def bank_account=(bank_account)
+        @bank_account=bank_account
+    end
+
+    def happiness=(happiness=Range.new(0,10, false) )
+        @happiness=happiness
+           
+    end
+
+    def hygiene=(hygiene)
+        @hygiene=hygiene
+    end
+
+    def clean
+        @hygiene >7 ? true : false
+    end
+end
